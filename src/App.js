018e3ca504e651105d1fbdcfaf1a138b53414062
Nicholas Feitel <nfeitel@bloomberg.net>
2022-12-18 18:47:11 -0500

    deliverable-2

--- src/App.js
+++ src/App.js
@@ -13,9 +13,6 @@ function App() {
   // DELIVERABLE 1: dogs will be an array
   const [dogs, setDogs] = useState([]);
 
-  // DELIVERABLE 3: example of filter state logic
-  const [filter, setFilter] = useState("all");
-
   // DELIVERABLE 1: fetch the dogs and set them to state
   // use the useEffect hook for fetches on load!
   useEffect(() => {
@@ -28,30 +25,6 @@ function App() {
     useEffect to only run once-ish on load
     */
   }, []);
-
-  // DELIVERABLE 3: Handle filtering for the dogs based on likes
-  const dogsToReturn = (dogs, filter) => {
-    if (filter === "liked") {
-      //return all dogs with likes
-      return dogs.filter((dog) => dog.likes > 0);
-    } else {
-      // if the filter says "all", return all dogs
-      return dogs;
-    }
-  };
-
-  // DELIVERABLE 3: BASIC
-  // variable for the filtered dogs
-  // comment out here and uncomment below to try that version!
-  const filteredDogs = dogsToReturn(dogs, filter);
-
-  /* 
-  DELIVERABLE 3: BONUS
-  useMemo is a good hook for filtered lists that 
-  caches stateful stuff that you have to do a lot of logic on!
-  */
-  // uncomment below and comment out the other filteredDogs above to try!
-  // const filteredDogs = useMemo(() => dogsToReturn(dogs, filter), [dogs, filter]);
 
   // DELIVERABLE 2: addLike, let's use linear search
   const addLike = (id, currentLikes) => {
@@ -75,13 +48,8 @@ function App() {
         {/* example of a drodown implementation */}
         <select
           onChange={(e) => {
-            /* 
-            DELIVERABLE 3: onChange is used for a dropdown and the value is the value of the option selected
-            the value of the dropdown should be the stateful value, i.e: filter!
-             */
-            setFilter(e.target.value);
+            console.log(e.target.value);
           }}
-          value={filter}
         >
           <option value="all">All Dogs</option>
           <option value="liked">Liked Dogs</option>
@@ -90,16 +58,10 @@ function App() {
         <br />
         {/* we use Array.prototype.map because an array of JSX object is valid react code */}
         <div id="dogs">
-          {/* DELIVERABLE 3: Check to see if there are any dogs to show */}
-          {/* we use a ternary to check whether all the currently filtered dogs have a length greater than 0 */}
-          {/* a length > 0 is truthy and less than 0 is falsy! */}
-          {filteredDogs.length ? (
-            filteredDogs.map((dog) => {
-              return <DogDiv dog={dog} addLike={addLike} />;
-            })
-          ) : (
-            <h2>No Dogs To Show ;(</h2>
-          )}
+          {/* DELIVERABLE 2: map out the dogs array to individual components */}
+          {dogs.map((dog) => {
+            return <DogDiv dog={dog} addLike={addLike} />;
+          })}
         </div>
       </div>
     </div>
